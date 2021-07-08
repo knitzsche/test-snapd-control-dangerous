0128c3909288ab05b90119f770d9f7a79a3048e7
Kyle Nitzsche <kyle.nitzsche@canonical.com>
2021-07-08 16:44:38 -0400

    Remove comment

--- doit/bin/doit.sh
+++ doit/bin/doit.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 ${SNAP}/usr/bin/curl -sS --unix-socket /run/snapd.socket http://localhost/v2/snaps
-
-# ${SNAP}/usr/bin/curl -o "${SNAP_RES_FILE}" -sS --unix-socket /run/snapd.socket --form snap=@${MNT}/${ASNAP} http://localhost/v2/snaps
